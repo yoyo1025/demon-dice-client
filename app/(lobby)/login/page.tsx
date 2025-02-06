2cd9fef31a013a79bfe9e71e16c38f19d8fedd4b
yoyo1025 <hsjejxys@gmail.com>
2025-02-06 17:30:49 +0900

    ログインフォームひな形完成

--- app/(lobby)/login/page.tsx
+++ app/(lobby)/login/page.tsx
@@ -1,21 +1,80 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
 export default function Login() {
   return (
-    // <div className="container mx-auto flex justify-center items-center text-center">
     <div className="h-screen w-screen flex justify-center items-center text-center">
-      <Tabs defaultValue="account">
-        <TabsList>
-          <TabsTrigger value="Login">Login</TabsTrigger>
-          <TabsTrigger value="Signup">Signup</TabsTrigger>
-        </TabsList>
-        <TabsContent value="Login">
-          おかえりなさい
-        </TabsContent>
-        <TabsContent value="Signup">
-          新しくゲームを始める
-        </TabsContent>
-      </Tabs>
+      <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="Login">Login</TabsTrigger>
+        <TabsTrigger value="Signup">Signup</TabsTrigger>
+      </TabsList>
+      <TabsContent value="Login">
+        <Card>
+          <CardHeader>
+            <CardTitle>ログイン</CardTitle>
+            <CardDescription>
+              おかえりなさい！ゲームを始めましょう！！
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email"/>
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>ログイン</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="Signup">
+        <Card>
+          <CardHeader>
+            <CardTitle>新規登録</CardTitle>
+            <CardDescription>
+              はじめまして！ゲームを始めましょう！！
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input id="current" type="text" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="text" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>新規登録</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
     </div>
   );
 }
