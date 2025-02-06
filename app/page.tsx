30311a302c67af7c296dbfdc3f072784ad21709e
yoyo1025 <hsjejxys@gmail.com>
2025-02-06 16:48:47 +0900

    shacn/uiインストール

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container mx-auto text-center">
       のとさんずをつかっています
+      <Button>Click me</Button>
     </div>
   );
 }
